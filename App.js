5d28f7ec33675a94c564235c5b9a1649351b3eec
hsimao <e087754958@gmail.com>
2021-11-17 21:05:35 +0800

    feat: 開啟通知音效

--- App.js
+++ App.js
@@ -4,6 +4,16 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import * as Notifications from "expo-notifications";
 import * as Permissions from "expo-permissions";
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: false
+    };
+  }
+});
+
 export default function App() {
   useEffect(() => {
     // 檢查通知權限, 是否已經允許 app 使用通知功能, 若無授權則重新詢問
@@ -24,7 +34,8 @@ export default function App() {
     Notifications.scheduleNotificationAsync({
       content: {
         title: "My first local notification",
-        body: "This is the first local notification we are sending!"
+        body: "This is the first local notification we are sending!",
+        sound: true // 開啟通知音效, 用戶需要有開啟聲音, 並且該 app 需要在背景執行, 才會有音效
       },
       trigger: {
         seconds: 10
